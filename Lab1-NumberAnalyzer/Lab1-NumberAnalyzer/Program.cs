6f8783a730f21fe052d51d575c549089e8730858
Eric Dopp <ericdopp@proton.me>
2023-07-15 03:02:14 -0400

    Adding BasicLoops Lab submission

--- Lab1-NumberAnalyzer/Lab1-NumberAnalyzer/Program.cs
+++ Lab1-NumberAnalyzer/Lab1-NumberAnalyzer/Program.cs
@@ -1,5 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.DataAnnotations;
 
-Console.WriteLine("Hola Mundo");
+Console.WriteLine("Hello, what is your name?");
+string userName = Console.ReadLine();
+Console.WriteLine("Hello " + userName);
+bool userChoice = true;
 
+while (userChoice)
+{
+    Console.WriteLine("Please enter a number between 1 and 100, inclusive.");
+    string userInput = Console.ReadLine();
+    int userNumber = int.Parse(userInput);
+    Console.WriteLine("Would you like to analyze your number?");
+    string userAnswer = Console.ReadLine();
+    if (userAnswer.ToLower() == "no")
+        userChoice = false;
+    else if (userAnswer.ToLower() == "yes")
+    {
+        userChoice = true;
+        if (userNumber > 100 || userNumber < 0)
+        {
+            Console.WriteLine("Please enter a number berween 1 and 100 bro.");
+        }
+        else if (userNumber % 2 == 1 && userNumber < 60)
+        {
+            Console.WriteLine(userNumber + " is odd and less than 60.");
+        }
+        else if ((userNumber % 2 == 0) && userNumber < 25)
+        {
+            Console.WriteLine(userNumber + " is even and less than 25.");
+        }
+        else if (userNumber % 2 == 0 && userNumber >= 26 && userNumber <= 60)
+        {
+            Console.WriteLine(userNumber + " is even and between 26 and 60 inclusive.");
+        }
+        else if (userNumber % 2 == 0 && userNumber > 60)
+        {
+            Console.WriteLine(userNumber + " is even and greater than 60.");
+        }
+        else if ((userNumber % 2 == 1) && userNumber > 60)
+        {
+            Console.WriteLine(userNumber + " is odd and greater than 60.");
+        }
+        else
+            Console.WriteLine("This isn't covered in my lab's requirements!");
+    }
+    else
+    {
+        Console.WriteLine("Please enter, 'Yes' or 'No'");
+    }
+}
---
+++ Lab2-BasicLoops/Lab2-BasicLoops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+bool userChoice = true;
+
+do
+{
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Would you like to continue (y/n)?");
+    string userInput = Console.ReadLine();
+    if (userInput == "n")
+    {
+        userChoice = false;
+    }
+    else;
+}
+while (userChoice);
