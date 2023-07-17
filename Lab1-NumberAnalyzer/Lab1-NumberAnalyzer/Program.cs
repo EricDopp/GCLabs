c4009dbc9d7a26d33e25a8fe7dbc550c9d585173
Eric Dopp <ericdopp@proton.me>
2023-07-17 02:36:50 -0400

    Submitting final entry

--- Lab1-NumberAnalyzer/Lab1-NumberAnalyzer/Program.cs
+++ Lab1-NumberAnalyzer/Lab1-NumberAnalyzer/Program.cs
@@ -1,21 +1,49 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-Console.WriteLine("Hello, what is your name?");
+﻿Console.WriteLine("Hello, what is your name?");
 string userName = Console.ReadLine();
 Console.WriteLine("Hello " + userName);
 bool userChoice = true;
+Console.WriteLine("Please enter a number between 1 and 100, inclusive.");
+string userInput = Console.ReadLine();
+int userNumber = int.Parse(userInput);
+if (userNumber > 100 || userNumber < 0)
+{
+    Console.WriteLine("Please enter a number berween 1 and 100 bro.");
+}
+else if (userNumber % 2 == 1 && userNumber < 60)
+{
+    Console.WriteLine(userNumber + " is odd and less than 60.");
+}
+else if ((userNumber % 2 == 0) && userNumber < 25)
+{
+    Console.WriteLine(userNumber + " is even and less than 25.");
+}
+else if (userNumber % 2 == 0 && userNumber >= 26 && userNumber <= 60)
+{
+    Console.WriteLine(userNumber + " is even and between 26 and 60 inclusive.");
+}
+else if (userNumber % 2 == 0 && userNumber > 60)
+{
+    Console.WriteLine(userNumber + " is even and greater than 60.");
+}
+else if ((userNumber % 2 == 1) && userNumber > 60)
+{
+    Console.WriteLine(userNumber + " is odd and greater than 60.");
+}
+else
+    Console.WriteLine("This isn't covered in my lab's requirements!");
+
 
 while (userChoice)
 {
-    Console.WriteLine("Please enter a number between 1 and 100, inclusive.");
-    string userInput = Console.ReadLine();
-    int userNumber = int.Parse(userInput);
-    Console.WriteLine("Would you like to analyze your number?");
+    Console.WriteLine("Would you like to stop?");
     string userAnswer = Console.ReadLine();
-    if (userAnswer.ToLower() == "no")
+    if (userAnswer.ToLower() == "yes")
         userChoice = false;
-    else if (userAnswer.ToLower() == "yes")
+    else if (userAnswer.ToLower() == "no")
     {
+        Console.WriteLine("Please enter a number between 1 and 100, inclusive.");
+        userInput = Console.ReadLine();
+        userNumber = int.Parse(userInput);
         userChoice = true;
         if (userNumber > 100 || userNumber < 0)
         {
