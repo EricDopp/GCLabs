931b707d8b14324ab20064a9ef21dc560de18444
Eric Dopp <ericdopp@proton.me>
2023-08-09 17:29:30 -0400

    Submitting CircleObjects lab

---
+++ Lab9-CircleObjects/Lab9-CircleObjects/Circle.cs
@@ -0,0 +1,58 @@
+﻿namespace Lab9_CircleObjects;
+
+public class Circle
+{
+    private double radius { get; set; }
+    public Circle(double radius)
+    {
+        this.radius = radius;
+        Console.WriteLine("");
+        Console.WriteLine($"The diameter is: {CalculateDiameter()}");
+        Console.WriteLine($"The circumference is: {CalculateCircumference()}");
+        Console.WriteLine($"The area is: {CalculateArea()}");
+        Console.WriteLine("");
+        Grow();
+    }
+    public double CalculateDiameter()
+    {
+        return this.radius * 2;
+    }
+    public double CalculateCircumference()
+    {
+        return CalculateDiameter() * Math.PI;
+    }
+    public double CalculateArea()
+    {
+        return Math.PI * (this.radius * this.radius);
+    }
+    public void Grow()
+    {
+        bool userContinue = true;
+        Console.Write("Should the circle grow? (y/n): ");
+        string userInput = Console.ReadLine();
+        while (userContinue)
+        {
+            if (userInput.ToLower() == "y")
+            {
+                Console.WriteLine("The circle is magically growing.");
+                this.radius += this.radius;
+                Circle circle = new Circle(this.radius);
+                break;
+            }
+            else if (userInput.ToLower() == "n")
+            {
+                Console.WriteLine($"Goodbye. The circle's final radius is {this.radius}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Please input 'y' or 'n'.");
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+    public static double GetRadius(double radius)
+    {
+        return radius;
+    }
+}
---
+++ Lab9-CircleObjects/Lab9-CircleObjects/Program.cs
@@ -0,0 +1,27 @@
+﻿using Lab9_CircleObjects;
+
+bool userContinue1 = true;
+bool userContinue2 = true;
+
+double userRadius = 0;
+
+Console.WriteLine("Welcome to the Circle Tester.");
+Console.Write("Enter radius: ");
+string userInput = Console.ReadLine();
+bool parseSucceeded = double.TryParse(userInput, out userRadius);
+
+while (userContinue1)
+{
+    if (userRadius <= 0 || (parseSucceeded = false))
+    {
+        Console.WriteLine("Please input a valid number greater than zero");
+        userInput = Console.ReadLine();
+        parseSucceeded = double.TryParse(userInput, out userRadius);
+    }
+    else
+    {
+        break;
+    }
+}
+Circle.GetRadius(userRadius);
+Circle circle = new Circle(userRadius);
