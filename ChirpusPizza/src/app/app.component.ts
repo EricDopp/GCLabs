333f45e050704f1be3b68abee2919de268ea9a1b
Eric Dopp <ericdopp@proton.me>
2023-11-08 17:12:47 -0500

    Submitting Chirpus Pizza lab

--- ChirpusPizza/src/app/app.component.ts
+++ ChirpusPizza/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './models/MenuItem';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ChirpusPizza';
+
+  MenuItems:MenuItem[] = [
+    { Name: "Chicken Fingers", Category: "dinner", Price: 11.99},
+    { Name: "Pizza", Category: "dinner", Price: 11.99},
+    { Name: "Wings", Category: "sides", Price: 8.99},
+    { Name: "Breadsticks", Category: "sides", Price: 4.99},
+    { Name: "Ceasar Salad", Category: "salads", Price: 5.99},
+    { Name: "Cinnamonk Roll", Category: "dessert", Price: 8.99},
+  ]
+
+  dinner: MenuItem[] = [];
+  getByCategory(category: string): MenuItem[] {
+    return this.MenuItems.filter(item => item.Category === category);
+  }
+
+  getCategories(): string[] {
+    return Array.from(new Set(this.MenuItems.map(item => item.Category)));
+  }
 }
